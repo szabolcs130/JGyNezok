0ba9a7d8f8051adc45a9b1711b4c0e9fb0f92287
szabolcs130 <ferencziszabolcs130@gmail.com>
2024-11-15 13:24:23 +0100

    Bejelentkezes regisztracio kilepes
    Bejelentkezes regisztracio kilepes

---
+++ JGyNezok/JGyNezok/src/main/java/com/example/JGyNezok/JGyNezokApplication.java
@@ -0,0 +1,13 @@
+package com.example.JGyNezok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JGyNezokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JGyNezokApplication.class, args);
+	}
+
+}
---
+++ JGyNezok/JGyNezok/src/test/java/com/example/JGyNezok/JGyNezokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JGyNezok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JGyNezokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
